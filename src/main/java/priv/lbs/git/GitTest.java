15bd97f68d26edea386522c6230b9398aeb8bd74
libisen <15630903591@163.com>
2024-04-13 22:51:17 +0800

    test push

--- src/main/java/priv/lbs/git/GitTest.java
+++ src/main/java/priv/lbs/git/GitTest.java
@@ -3,5 +3,6 @@ package priv.lbs.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("test push");
     }
 }
