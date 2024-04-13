62340fa58ab7ed261a6eb537668da39024a90c5c
Anonymity <141937183+LiBisen@users.noreply.github.com>
2024-04-13 22:57:04 +0800

    pull test

--- src/main/java/priv/lbs/git/GitTest.java
+++ src/main/java/priv/lbs/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("test push");
         System.out.println("test push2");
+        System.out.println("test pull");
     }
 }
